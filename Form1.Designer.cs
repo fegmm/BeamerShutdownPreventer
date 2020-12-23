9d8845d129f54e7769a66da851f4b1202fe8545f
Samuel Weber <samuel@familie-weber.de>
2020-12-23 15:18:18 +0100

    Finished Code

---
+++ Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace BeamerShutdownPreventer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(120, 0);
+            this.ControlBox = false;
+            this.Enabled = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Erinnerung an laufenden Beamer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net;
+
+namespace BeamerShutdownPreventer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public extern static bool ShutdownBlockReasonCreate(IntPtr hWnd, [MarshalAs(UnmanagedType.LPWStr)] string pwszReason);
+
+        protected override void WndProc(ref Message aMessage)
+        {
+            const int WM_QUERYENDSESSION = 0x0011;
+            const int WM_ENDSESSION = 0x0016;
+
+            if ((aMessage.Msg == WM_QUERYENDSESSION || aMessage.Msg == WM_ENDSESSION))
+            {
+                if (IsBeamerOn().Result)
+                {
+                    return;
+                }
+            }
+            base.WndProc(ref aMessage);
+        }
+
+        private async Task<bool> IsBeamerOn()
+        {
+            try
+            {
+                var client = new HttpClient(new HttpClientHandler() { Credentials = new NetworkCredential("EPSONWEB", "FegMM", "Web Control"), ServerCertificateCustomValidationCallback = (_, _, _, _) => true });
+                var result = await client.PostAsync("https://192.168.1.31/cgi-bin/webconf", new FormUrlEncodedContent(new Dictionary<string, string>() { { "page", "70" } }));
+                var resultString = await result.Content.ReadAsStringAsync();
+                return resultString.Contains("\"laserstatus\": \"On\"");
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShutdownBlockReasonCreate(this.Handle, "BEAMER IST NOCH AN! - Dennoch herunterfahren?");
+        }
+    }
+}
